968e2f53d5c50c23c4b2998ac3337102890f455e
berkinory <berk@mirac.dev>
2025-11-12 17:50:39 +0300

    new design system...

--- web/src/app/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function DashboardPage() {
-  return (
-    <div>
-      <h1 className="font-bold text-2xl">Dashboard</h1>
-      <p className="mt-4">Page content goes here</p>
-    </div>
-  );
-}
